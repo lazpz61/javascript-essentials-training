f862fe7b31fc38f8293ff0a7d3fb9652439e53a7
LazaroMPerez <lazpz61@gmail.com>
2021-06-17 18:38:25 -0400

    edit

---
+++ .vscode/index.js
@@ -0,0 +1,21 @@
+let bird = {
+  genus: "corvus",
+  species: "corvax",
+  commonName: "raven",
+  callType: "squaky",
+  quote: "nevermore",
+  maxOffspring: 5,
+  noisy: true,
+  deadly: false,
+};
+
+let bear = {
+  genus: "goog",
+  species: "lion",
+  commonName: "leo",
+  callType: "raw",
+  quote: "king",
+  maxOffspring: 10,
+  noisy: true,
+  deadly: false,
+};
